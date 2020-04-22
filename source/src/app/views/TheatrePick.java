18c39bfcd7b4f61a5fc5bb3333627efc5444b22d
Margarita <eightnum.81@gmail.com>
2020-04-22 13:14:55 +0300

    change ui theme

--- source/src/app/views/TheatrePick.java
+++ source/src/app/views/TheatrePick.java
@@ -8,23 +8,26 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import app.controllers.TheatreController;
+import mdlaf.animation.MaterialUIMovement;
+import mdlaf.utils.MaterialColors;
 
 @SuppressWarnings("serial")
 public class TheatrePick extends JPanel {
-    
+
     private JButton lavinaMultiplex;
 
     public TheatrePick() {
-        
+
         super();
         setLayout(new FlowLayout(FlowLayout.CENTER));
 
         lavinaMultiplex = new JButton("Lavina Multiplex");
+        lavinaMultiplex.setFocusable(false);
 
+        MaterialUIMovement.add(lavinaMultiplex, MaterialColors.GRAY_100);
         add(lavinaMultiplex);
 
         new TheatreController(this).initController();
-
     }
 
     public JButton getLavinaMultiplex() {
@@ -33,7 +36,7 @@ public class TheatrePick extends JPanel {
     }
 
     public JFrame getParentFrame() {
-        
-        return (JFrame) SwingUtilities.getRoot(this); 
+
+        return (JFrame) SwingUtilities.getRoot(this);
     }
 }
