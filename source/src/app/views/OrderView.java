0146e66ec4771957f861090616455725568b950b
Margarita <eightnum.81@gmail.com>
2020-04-27 01:30:36 +0300

    init order view

--- source/src/app/views/OrderView.java
+++ source/src/app/views/OrderView.java
@@ -7,12 +7,21 @@ import javax.swing.SwingUtilities;
 @SuppressWarnings("serial")
 public class OrderView extends JPanel {
 
+    private MainOrder mainOrder; 
+
     public OrderView() {
 
+        mainOrder = new MainOrder();
+        add(mainOrder);
     }
 
     public JFrame getParentFrame() {
 
         return (JFrame) SwingUtilities.getRoot(this);
+    }
+
+    public MainOrder getMainOrder() {
+
+        return mainOrder;
     }
 }
